52feea25686c74959df8406eda822e4b20cc458d
khanshai9050 <167965233+khanshai9050@users.noreply.github.com>
2024-06-03 16:28:25 +0530

    Add files via upload

---
+++ SRVSScript.js
@@ -0,0 +1,58 @@
+/*JS for preloader*/
+var loader=document.getElementById("preloader");
+window.addEventListener("load",function(){
+		loader.style.display = "none";
+		})
+
+
+// Get the top to up button://
+let mybutton = document.getElementById("myScrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}	
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+//end of Get the top to up button://
+
+/*js for work counter*/
+$(document).ready(function () {
+        $.fn.jQuerySimpleCounter = function (options) {
+            var settings = $.extend({
+                start: 0,
+                end: 100,
+                easing: 'swing',
+                duration: 400,
+                complete: ''
+            }, options);
+
+            var thisElement = $(this);
+
+            $({ count: settings.start }).animate({ count: settings.end }, {
+                duration: settings.duration,
+                easing: settings.easing,
+                step: function () {
+                    var mathCount = Math.ceil(this.count);
+                    thisElement.text(mathCount);
+                },
+                complete: settings.complete
+            });
+        };
+        $('#number1').jQuerySimpleCounter({ end: 8000, duration: 5000 });
+        $('#number2').jQuerySimpleCounter({ end: 100, duration: 5000 });
+        $('#number3').jQuerySimpleCounter({ end: 3000, duration: 5000 });
+        $('#number4').jQuerySimpleCounter({ end: 2000, duration: 5000 });
+		
+    });
+/*end of js for work counter*/
